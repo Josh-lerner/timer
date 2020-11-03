fa3c1fdfdbc0e7d9ace3bbcec606c03c18e48e65
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-03 15:25:49 -0500

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,9 +1,13 @@
-const seconds = [10, 3, 5, 9,];
+var myAlarms = process.argv.slice(2);
+console.log('myAlarms: ', myAlarms);
 
-const alarmClock = function(seconds) {
-  for (const second of seconds) {
-    setTimeout(() => { process.stdout.write('ding\n');}, second * 1000);
-  }
-};
 
-alarmClock(seconds)
+const alarmClock = function(myAlarms) {
+  
+setTimeout(() => { process.stdout.write('ding\n');}, myAlarms[0] * 1000);  
+setTimeout(() => { process.stdout.write('ding\n');}, myAlarms[1] * 1000);
+setTimeout(() => { process.stdout.write('ding\n');}, myAlarms[2] * 1000);
+setTimeout(() => { process.stdout.write('ding\n');}, myAlarms[3] * 1000);
+setTimeout(() => { process.stdout.write('ding\n');}, myAlarms[4] * 1000);
+}
+ alarmClock(myAlarms)
